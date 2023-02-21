33798e96b573082c162cad15d6bdb58482ca13e7
wangxi <wangxi@360shuke.com>
2023-02-21 10:16:10 +0800

    更新到alist v3

--- embyAlist/nginx/conf.d/emby.js
+++ embyAlist/nginx/conf.d/emby.js
@@ -2,25 +2,21 @@
 async function redirect2Pan(r) {
     //根据实际情况修改下面4个设置
     const embyHost = 'http://172.17.0.1:8096'; //这里默认emby/jellyfin的地址是宿主机,要注意iptables给容器放行端口
-    // rclone 的挂载目录, 例如将od, gd挂载到/mnt目录下:  /mnt/onedrive  /mnt/gd ,那么这里 就填写 /mnt//
-    // 这里直接会读取alist的挂载目录，所以不在填写本地的挂载目录
+    const embyMountPath = '/home';  // rclone 的挂载目录, 通过df -h 查看例如将od, gd挂载到/mnt目录下:  /mnt/onedrive  /mnt/gd ,那么这里 就填写 /mnt
     const alistPwd = '';      //alist password
-    const alistApiPath = 'http://172.17.0.1:5244/api/public/path'; //访问宿主机上5244端口的alist api, 要注意iptables给容 器放行端口
+    const alistAddr = 'http://172.17.0.1:5244'; //访问宿主机上5244端口的alist api, 要注意iptables给容 器放行端口
+    const embyApiKey = '6ceb72bebf034b898'
 
     //fetch mount emby/jellyfin file path
     const regex = /[A-Za-z0-9]+/g;
     const itemId = r.uri.replace('emby', '').replace(/-/g, '').match(regex)[1];
-    const mediaSourceId = r.args.MediaSourceId;
-    let api_key = r.args.api_key;
-    // jellyfin 必须要用到userid，不然接口会报错，这里用固定的一个id只是用于获取剧集信息，不会影响用户其他信息
-    const userId = ''
+    const mediaSourceId =  r.args.MediaSourceId ? r.args.MediaSourceId : r.args.mediaSourceId;
+    let api_key = r.args['X-Emby-Token'] ? r.args['X-Emby-Token'] : r.args.api_key;
+    api_key = api_key ? api_key : embyApiKey;
 
-    //infuse用户需要填写下面的api_key, 感谢@amwamw968
-    if ((api_key === null) || (api_key === undefined)) {
-        api_key = '';//这里填自己的emby/jellyfin API KEY
-        r.warn(`api key for Infuse: ${api_key}`);
-    }
-    // r.warn(`r=: ${JSON.stringify(r)}`);
+    // jellyfin 必须要用到userid，不然接口会报错，这里用固定的一个id只是用于获取剧集信息，不会影响用户其他信息
+    const userId = '000b9695c0824bdba'
+
     //请求jellyfin接口来获取item的信息
     const itemInfoUri = `${embyHost}/Items/${itemId}/PlaybackInfo?MediaSourceId=${mediaSourceId}&api_key=${api_key}&userId=${userId}`;
     r.warn(`itemInfoUri: ${itemInfoUri}`);
@@ -31,24 +27,29 @@ async function redirect2Pan(r) {
         return;
     }
     r.warn(`mount emby file path: ${embyRes}`);
+
     //fetch alist direct link
-    const alistFilePath = embyRes;
+    // 替换挂载根目录embyMountPath
+    const alistFilePath = embyRes.replace(embyMountPath, '');
     //第一次尝试请求alist的api获取真实播放路径
-    const alistRes = await fetchAlistPathApi(alistApiPath, alistFilePath, alistPwd, r);
+    const alistFsGetApiPath = `${alistAddr}/api/fs/get`;
+    const alistRes = await fetchAlistPathApi(alistFsGetApiPath, alistFilePath, alistPwd, r);
     if (!alistRes.startsWith('error')) {
         r.warn(`redirect to: ${alistRes}`);
         r.return(302, alistRes);
         return;
     }
-    if (alistRes.startsWith('error401')) {
+    if (alistRes.startsWith('error403')) {
         r.error(alistRes);
-        r.return(401, alistRes);
+        r.return(403, alistRes);
         return;
     }
-    if (alistRes.startsWith('error404')) {
+    if (alistRes.startsWith('error500')) {
         const filePath = alistFilePath.substring(alistFilePath.indexOf('/', 1));
+        r.warn(`filePath 路径：${filePath}`);
         //获取alist跟目录，拿到我们挂载的所有网盘目录
-        const foldersRes = await fetchAlistPathApi(alistApiPath, '/', alistPwd, r);
+        const alistFsListApiPath = `${alistAddr}/api/fs/list`;
+        const foldersRes = await fetchAlistPathApi(alistFsListApiPath, '/', alistPwd, r);
         if (foldersRes.startsWith('error')) {
             r.error(foldersRes);
             r.return(500, foldersRes);
@@ -56,11 +57,11 @@ async function redirect2Pan(r) {
         }
         //如果有多个网盘，会以数组的形式来返回多个跟目录，这里循环进行请求
         const folders = foldersRes.split(',').sort();
+        r.warn(`alist网盘列表：${folders}`);
         for (let i = 0; i < folders.length; i++) {
             r.warn(`try to fetch alist path from /${folders[i]}${filePath}`);
             //直接请求网盘跟目录下的路径来获取
-            // const driverRes = await fetchAlistPathApi(alistApiPath, `/${folders[i]}${filePath}`, alistPwd, r);
-            const driverRes = await fetchAlistPathApi(alistApiPath, `${filePath}`, alistPwd, r);
+            const driverRes = await fetchAlistPathApi(alistFsGetApiPath, `/${folders[i]}${filePath}`, alistPwd, r);
             if (!driverRes.startsWith('error')) {
                 r.warn(`redirect to: ${driverRes}`);
                 r.return(302, driverRes);
@@ -99,23 +100,17 @@ async function fetchAlistPathApi(alistApiPath, alistFilePath, alistPwd, r) {
                 return `error: alist_path_api response is null`;
             }
             if (result.message == 'success') {
-                if (result.data.type == 'file') {
-                    return result.data.files[0].url;
+                // alist v3 
+                if(result.data.raw_url) {
+                    return result.data.raw_url;
                 }
-                if (result.data.type == 'folder') {
-                    return result.data.files.map(item => item.name).join(',');
-                }
+                // 没有的话就按照目录来处理
+                return result.data.content.map(item => item.name).join(',');
             }
-            if (result.code == 401) {
-                return `error401: alist_path_api ${result.message}`;
+            if (result.code == 403) {
+                return `error403: alist_path_api ${result.message}`;
             }
-            if (result.message.includes('account')) {
-                return `error404: alist_path_api ${result.code} ${result.message}`;
-            }
-            if (result.message == 'file not found' || result.message == 'path not found') {
-                return `error404: alist_path_api ${result.message}`;
-            }
-            return `error: alist_path_api ${result.code} ${result.message}`;
+            return `error500: alist_path_api ${result.code} ${result.message}`;
         }
         else {
             return `error: alist_path_api ${response.status} ${response.statusText}`;
